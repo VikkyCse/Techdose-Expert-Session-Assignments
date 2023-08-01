90a08a1236fc1033ae54ab75e2203789fca408e6
VikkyCse <126576893+VikkyCse@users.noreply.github.com>
2023-08-01 22:51:06 +0530

    Create  Kth Largest Element in an Array.py

---
+++ Array/Kth Largest Element in an Array.py
@@ -0,0 +1,14 @@
+#Leetcode 215 - Kth Largest Element in an Array
+
+class Solution:
+    def findKthLargest(self, nums, k):
+        pivot = nums[0]
+        
+        left = [num for num in nums if num < pivot]
+        equal = [num for num in nums if num == pivot]
+        right = [num for num in nums if num > pivot]
+        
+        if k <= len(right): return self.findKthLargest(right, k)
+        elif len(right) < k <= len(right) + len(equal): return equal[0]
+        else: return self.findKthLargest(left, k - len(right) - len(equal))
+
